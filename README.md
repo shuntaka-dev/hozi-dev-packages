c9ac945ba2df1b18a3d262fd7097515b460c0d53
shuntaka9576 <shuntaka9576@gmail.com>
2021-04-17 13:32:26 +0900

    chore: dry-runする環境を構築

---
+++ README.md
@@ -0,0 +1,10 @@
+# hozi-dev-packages
+## preview
+```bash
+lerna run build -- --watch
+```
+
+```bash
+cd dry-run
+yarn dev
+```
---
+++ dry-run/pages/index.tsx
@@ -0,0 +1,43 @@
+import styles from '../styles/Home.module.css'
+import * as hoziDevMarkdown from '../../packages/markdown-to-html/build/src';
+import * as He from 'he';
+import fs from 'fs';
+import {useState} from 'react';
+
+export const getStaticProps = async () => {
+  const testArticleData = fs.readFileSync('./testData/articles/1.md')
+  const html = hoziDevMarkdown.convertToHoziDevHtml(testArticleData.toString())
+
+  return {
+    props: {
+      html: html,
+    },
+    revalidate: 1,
+  };
+};
+
+const Home = ({ html }) => {
+  const [color, setColor] = useState<string>('light');
+
+  return (
+    <div className={styles.editor}>
+      <div className={styles.content}>
+        <div className="hozi-dev-article-content">
+          <div
+            className={`hozi-dev-article-content-${color}`}
+            dangerouslySetInnerHTML={{
+              __html: He.unescape(html),
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className={styles.rightSideBar}>
+        color: <button className={styles.colorButton} onClick={() => {
+          color === 'light' ? setColor('dark') : setColor('light')
+        }}>{color === 'light' ? 'dark' : 'light'}</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
